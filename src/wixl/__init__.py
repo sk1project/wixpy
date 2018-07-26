e68359fdf0685345071525fc4efaac236c221a0c
Igor Novikov <sk1.project.org@gmail.com>
2018-07-26 17:06:18 +0300

    reformatting

--- src/wixl/__init__.py
+++ src/wixl/__init__.py
@@ -2,20 +2,20 @@
 #
 #   MSW MSI builder
 #
-# 	Copyright (C) 2018 by Igor E. Novikov
+#   Copyright (C) 2018 by Igor E. Novikov
 #
-# 	This program is free software: you can redistribute it and/or modify
-# 	it under the terms of the GNU General Public License as published by
-# 	the Free Software Foundation, either version 3 of the License, or
-# 	(at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 #
-# 	This program is distributed in the hope that it will be useful,
-# 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-# 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# 	GNU General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-# 	You should have received a copy of the GNU General Public License
-# 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Supported features (WiX & wixl):
@@ -53,7 +53,7 @@ def _normalize_path(path):
     return os.path.abspath(os.path.expanduser(path))
 
 
-def build(json_data, xml_only=False, engine=PYWIXL_ENGINE, 
+def build(json_data, xml_only=False, engine=PYWIXL_ENGINE,
           encoding='utf-8', stdout=False):
     wixutils.DEFAULT_ENCODING = encoding
     json_data['_pkgname'] = PROJECT
