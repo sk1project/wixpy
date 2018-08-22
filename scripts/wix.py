62794026d9147c3438d392d6a667391191235c45
Igor E. Novikov <sk1.project.org@gmail.com>
2018-08-23 00:42:05 +0300

    shebang fix

--- scripts/wix.py
+++ scripts/wix.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
 #
 #   WiX MSI builder script
@@ -18,10 +18,10 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import json
 import os
 import sys
 
+import json
 import wixpy
 
 HELP_TEMPLATE = '''
