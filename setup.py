219b6ac671f01c054ca0425eca99c2fdcd40a7a8
igor <sk1.project.org@gmail.com>
2018-08-26 21:42:41 +0300

    app data refactored

--- setup.py
+++ setup.py
@@ -33,49 +33,19 @@ CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 import wixpy
 
-NAME = wixpy.PROJECT
-VERSION = wixpy.VERSION
-DESCRIPTION = 'Crossplatform MSI builder'
-AUTHOR = 'Igor E. Novikov'
-AUTHOR_EMAIL = 'sk1.project.org@gmail.com'
-MAINTAINER = AUTHOR
-MAINTAINER_EMAIL = AUTHOR_EMAIL
-LICENSE = 'GPL v3'
-URL = 'https://wix.sk1project.net'
-DOWNLOAD_URL = URL
-CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Operating System :: POSIX :: Linux',
-    'Operating System :: Microsoft :: Windows',
-    'Programming Language :: Python :: 2.7',
-    "Topic :: Software Development :: Build Tools",
-]
-LONG_DESCRIPTION = '''
-WiX.Py is an open source crossplatform alternative for WiX toolset.
-Application allows building MSI packages as under MSW and Linux
-platforms including inside Docker environment. Unlike WiX/wixl applications
-WiX.Py is an JSON-driven builder. To build MSI package you need filling
-small JSON file only, describing generic application data (name, version etc.)
-
-Copyright (C) %s sK1 Project Team (https://wix.sk1project.net)
-''' % str(datetime.date.today().year)
-
 setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    maintainer=MAINTAINER,
-    maintainer_email=MAINTAINER_EMAIL,
-    license=LICENSE,
-    url=URL,
-    download_url=DOWNLOAD_URL,
-    long_description=LONG_DESCRIPTION,
-    classifiers=CLASSIFIERS,
+    name=wixpy.PROJECT,
+    version=wixpy.VERSION,
+    description=wixpy.DESCRIPTION,
+    author=wixpy.AUTHOR,
+    author_email=wixpy.AUTHOR_EMAIL,
+    maintainer=wixpy.MAINTAINER,
+    maintainer_email=wixpy.MAINTAINER_EMAIL,
+    license=wixpy.LICENSE,
+    url=wixpy.URL,
+    download_url=wixpy.DOWNLOAD_URL,
+    long_description=wixpy.LONG_DESCRIPTION,
+    classifiers=wixpy.CLASSIFIERS,
     packages=['wixpy'],
     package_dir={'wixpy': 'src/wixpy'},
     scripts=['scripts/%s/wix.py.exe' % WIN_ARCH] if os.name == 'nt'
